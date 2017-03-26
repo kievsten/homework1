4691f8f759774f3b9765e9d96600b4a5b67cd724
kievsten <kievsten@gmail.com>
2017-03-26 14:03:05 +0300

    The first task

---
+++ Main.java
@@ -0,0 +1,32 @@
+	package com.gmail.kievsten;
+	import java.util.*;
+	public class Main {
+	
+		public static void main(String[] args) {
+			
+			//Input Output Method
+			
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Input digit: ");
+			int x = sc.nextInt();
+			System.out.println("Digit: "+x);
+			
+			//Creating and initializing variables
+			
+			int a = x/10000;
+			int b = x%10000/1000;
+			int c = x%1000/100;
+			int d = x%100/10;
+			int e = x%10/1;
+			
+			//Output to the console
+			
+			System.out.println(a);
+			System.out.println(b);
+			System.out.println(c);
+			System.out.println(d);
+			System.out.println(e);
+	
+		}
+	
+	}
