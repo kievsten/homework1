42d53f0e7250d572846d78526b043da27154ab92
kievsten <kievsten@gmail.com>
2017-03-26 14:52:01 +0300

    Add files via upload

---
+++ TheFourthTask.java
@@ -0,0 +1,17 @@
+package com.gmail.kievsten;
+import java.util.*;
+public class TheFourthTask {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int c = 12;
+		int d = 13;
+		
+		int ACDC;
+		
+		ACDC = (int) (c*(Math.pow(16, 0))+(d*(Math.pow(16, 1)))+(c*(Math.pow(16, 2)))+(a*(Math.pow(16, 3))));
+		System.out.println("Encrypted number is: "+ACDC);
+	}
+
+}
